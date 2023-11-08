8ad849e72b90cc526c89f9926b9652d10ac044e0
sQlsmit <smit0421@outlook.com>
2023-11-07 21:08:19 -0600

    FirstJavaProject

---
+++ src/parameterconcept/CalculateSum.java
@@ -0,0 +1,12 @@
+package parameterconcept;
+
+public class CalculateSum {
+       public static void Sum(int a,int b) {
+    	   int sum=a+b;
+    	   System.out.println(sum);
+       }
+       public static void main(String[] args) {
+    	   Sum(3,5);
+       }
+}
+
